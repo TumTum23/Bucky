c77b99beab1f10931725510f227c944075788c5e
Joseph Noott <Jnootty@hotmail.com>
2018-12-06 18:26:28 +0100

    Amin area added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users, controllers: { registrations: 'users/registrations' }
   root 'tiles#index'
   resources :tiles
+
+
 end
